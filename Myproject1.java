fb722ed2e96f1661f6eff07eee03511dd11920c1
papangkorn <butter0026@gmail.com>
2023-03-22 11:18:51 +0700

    first commit

--- Myproject1.java
+++ Myproject1.java
@@ -4,7 +4,7 @@ package myproject1;
 public class Myproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello");
     }
     
 }
